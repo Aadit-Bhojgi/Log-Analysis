3c68dff414b285f5872aefaf58003563458d02eb
Aadit <aaditbhojgi@gmail.com>
2017-06-21 04:28:36 +0530

    Added Comments

--- Project_Tool.py
+++ Project_Tool.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+"""So I have made this file out of the scope of this project. 
+This file will work as a server, you just have to run this file
+and then go to your browser and type 'https://localhost:8000/
+NOTE(Though I have also provided the text file of the output & 
+the changes required in Project_Tool_DB.py is there in comments)
+"""
 
 from flask import Flask
 
@@ -6,6 +12,7 @@ from Project_Tool_DB import query_1, query_2, query_3
 
 app = Flask(__name__)
 
+# HTML wraping
 html = '''\
 <!DOCTYPE html>
 <html>
@@ -30,6 +37,8 @@ html = '''\
 '''
 
 
+# Calling the queries run by Project_tool_DB.py file
+# and appending them into the HTML Page
 @app.route('/', methods=['GET'])
 def main():
     """Main page of QUERY"""
--- Project_Tool_DB.py
+++ Project_Tool_DB.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
-
+"""This is the file connecting us with the database in our 
+system For now the queries will be sent to the server File
+(Project_Tool_DB.py) to print these files jus run the COMMAND 
+below file, the code you need is in the comments Below Though 
+I have also provided a text File of the Outputs.
+NOTE: (It will Take some time to open your localhost link
+because of the Queries.)
+Command: python Project_Tool_DB.py > Result.txt
+Run This command after Making the above changes.
+"""
 import psycopg2
 
 Db_name = "vagrant"
@@ -9,6 +18,7 @@ def query_1():
     """Return query 1 from the 'DATABASE: vagrant'."""
     db = psycopg2.connect(database=Db_name)
     c = db.cursor()
+    # article_view here is a VIEW (Query for this is in README.md)
     c.execute("""SELECT * FROM article_view LIMIT 3""")
     query_result = c.fetchall()
     db.close()
@@ -16,13 +26,14 @@ def query_1():
     for (title, count) in query_result:
         data = data + "Article: '{}', Views: {}<br>".format(title, count)
     return data
-    #print data
+    # print data
 
 
 def query_2():
     """Return query 2 from the 'DATABASE: vagrant'."""
     db = psycopg2.connect(database=Db_name)
     c = db.cursor()
+    # query_2 here is a VIEW (Query for this is in README.md)
     c.execute("""SELECT * FROM query_2""")
     query_result = c.fetchall()
     db.close()
@@ -30,19 +41,20 @@ def query_2():
     for (title, count) in query_result:
         data = data + "Author: '{}', Views: {}<br>".format(title, count)
     return data
-    #print data
+    # print data
 
 
 def query_3():
     """Return query 2 from the 'DATABASE: vagrant'."""
     db = psycopg2.connect(database=Db_name)
     c = db.cursor()
+    # query_3 here is a VIEW (Query for this is in README.md)
     c.execute("""SELECT * FROM query_3""")
     query_result = c.fetchall()
     db.close()
     data = "Date: {}, Percentage: {}%".format(query_result[0][0], query_result[0][1])
     return data
-    #print data
+    # print data
 
 
 """print "Q1. What are the most popular three articles of all time?\n"
